3578cf9ce9bee3ec121714ff3650085acb160cc6
Oscar <oscstr-9@student.ltu.se>
2020-09-24 17:09:52 +0200

    textures

--- engine/render/VERTEX.H
+++ engine/render/VERTEX.H
@@ -7,12 +7,15 @@ struct Vertex
 {
 	VectorMath3 pos;
 	VectorMath4 color;
+	float texture[2];
 	//-----------------------//
-	Vertex(VectorMath3 pos, VectorMath4 color);
+	Vertex(VectorMath3 pos, VectorMath4 color, float texX, float texY);
 };
 
-inline Vertex::Vertex(VectorMath3 posIn, VectorMath4 colorIn) {
+inline Vertex::Vertex(VectorMath3 posIn, VectorMath4 colorIn, float texX, float texY) {
 	pos = posIn;
 	color = colorIn;
+	texture[0] = texX;
+	texture[1] = texY;
 }
 #endif // !VERTICES_H
